f656539e0f9ef0c2113796c6e9c2bfe495937884
Franklin van de Meent <fr@nkl.in>
2013-04-14 00:15:08 +0200

    Functions are now directly added to module.exports

--- artsholland.js
+++ artsholland.js
@@ -8,170 +8,167 @@ License:  Unlicense / Public Domain
 var http = require('http'),
     querystring = require('querystring')
 
-var app = {
+module.exports.api: {
+	key:	''
+}
 
-	api: {
-		key:	''
-	},
+// Event
+module.exports.event = function( one, two, three ) {
 	
-	// Event
-	event: function( one, two, three ) {
+	if( typeof one == 'function' ) {
 		
-		if( typeof one == 'function' ) {
+		// app.event( oneCallback )
+		talk( 'event', one )
+		
+	} else if( typeof one == 'object' && typeof two == 'function' ) {
+		
+		// app.event( oneFilter, twoCallback )
+		talk( 'event', one, two )
+		
+		
+	} else if( typeof one == 'string' && typeof two == 'function' ) {
+		
+		// app.event( oneCIDN, twoCallback )
+		talk( 'event/'+ one, two )
+		
+	} else if( typeof one == 'string' && two == undefined ) {
+		
+		// var event = app.event( oneCIDN )
+		return {
 			
-			// app.event( oneCallback )
-			talk( 'event', one )
+			// app.event( oneCIDN ).venue( fourCallback )
+			venue: function( four ) {
+				talk( 'event/'+ one +'/venue', four )
+			},
 			
-		} else if( typeof one == 'object' && typeof two == 'function' ) {
+			// app.event( oneCIDN ).production( fourCallback )
+			production: function( four ) {
+				talk( 'event/'+ one +'/production', four )
+			},
 			
-			// app.event( oneFilter, twoCallback )
-			talk( 'event', one, two )
+			// app.event( oneCIDN ).room( fourCallback )
+			room: function( four ) {
+				talk( 'event/'+ one +'/room', four )
+			},
 			
+			// app.event( oneCIDN ).attachment( fourCallback )
+			attachment: function( four ) {
+				talk( 'event/'+ one +'/attachment', four )
+			},
 			
-		} else if( typeof one == 'string' && typeof two == 'function' ) {
-			
-			// app.event( oneCIDN, twoCallback )
-			talk( 'event/'+ one, two )
-			
-		} else if( typeof one == 'string' && two == undefined ) {
-			
-			// var event = app.event( oneCIDN )
-			return {
-				
-				// app.event( oneCIDN ).venue( fourCallback )
-				venue: function( four ) {
-					talk( 'event/'+ one +'/venue', four )
-				},
-				
-				// app.event( oneCIDN ).production( fourCallback )
-				production: function( four ) {
-					talk( 'event/'+ one +'/production', four )
-				},
-				
-				// app.event( oneCIDN ).room( fourCallback )
-				room: function( four ) {
-					talk( 'event/'+ one +'/room', four )
-				},
-				
-				// app.event( oneCIDN ).attachment( fourCallback )
-				attachment: function( four ) {
-					talk( 'event/'+ one +'/attachment', four )
-				},
-				
-				offering: function( four ) {
-					if( typeof four == 'function' ) {
+			offering: function( four ) {
+				if( typeof four == 'function' ) {
+					
+					// app.event( oneCIDN ).offering( fourCallback )
+					talk( 'event/'+ one +'/offering', four )
+					
+				} else if( typeof four == 'string' ) {
+					return {
 						
-						// app.event( oneCIDN ).offering( fourCallback )
-						talk( 'event/'+ one +'/offering', four )
-						
-					} else if( typeof four == 'string' ) {
-						return {
-							
-							// app.event( oneCIDN ).offering( fourName ).price( fiveCallback )
-							price: function( fcb ) {
-								talk( 'event/'+ one +'/offering/'+ four +'/price', five )
-							}
-							
+						// app.event( oneCIDN ).offering( fourName ).price( fiveCallback )
+						price: function( fcb ) {
+							talk( 'event/'+ one +'/offering/'+ four +'/price', five )
 						}
+						
 					}
 				}
 			}
 		}
-	},
-	
-	// Venue
-	venue: function( one, two, three ) {
-		
-		if( typeof one == 'function' ) {
-			
-			// app.venue( oneCallback )
-			talk( 'venue', one )
-			
-		} else if( typeof one == 'object' && typeof two == 'function' ) {
-			
-			// app.venue( oneFilter, twoCallback )
-			talk( 'venue', one, two )
-			
-			
-		} else if( typeof one == 'string' && typeof two == 'function' ) {
-			
-			// app.venue( oneCIDN, twoCallback )
-			talk( 'venue/'+ one, two )
-			
-		} else if( typeof one == 'string' && two == undefined ) {
-			
-			// var venue = app.venue( oneCIDN )
-			return {
-				
-				// app.venue( oneCIDN ).event( fourCallback )
-				event: function( four ) {
-					talk( 'venue/'+ one +'/event', four )
-				},
-				
-				// app.venue( oneCIDN ).production( fourCallback )
-				production: function( four ) {
-					talk( 'venue/'+ one +'/production', four )
-				},
-				
-				// app.venue( oneCIDN ).room( fourCallback )
-				room: function( four ) {
-					talk( 'venue/'+ one +'/room', four )
-				},
-				
-				// app.venue( oneCIDN ).attachment( fourCallback )
-				attachment: function( four ) {
-					talk( 'venue/'+ one +'/attachment', four )
-				}
-				
-			}
-		}
-	},
-	
-	// Production
-	production: function( one, two, three ) {
-		
-		if( typeof one == 'function' ) {
-			
-			// app.production( oneCallback )
-			talk( 'production', one )
-			
-		} else if( typeof one == 'object' && typeof two == 'function' ) {
-			
-			// app.production( oneFilter, twoCallback )
-			talk( 'production', one, two )
-			
-			
-		} else if( typeof one == 'string' && typeof two == 'function' ) {
-			
-			// app.production( oneCIDN, twoCallback )
-			talk( 'production/'+ one, two )
-			
-		} else if( typeof one == 'string' && two == undefined ) {
-			
-			// var production = app.production( oneCIDN )
-			return {
-				
-				// app.production( oneCIDN ).event( fourCallback )
-				event: function( four ) {
-					talk( 'production/'+ one +'/event', four )
-				},
-				
-				// app.production( oneCIDN ).production( fourCallback )
-				venue: function( four ) {
-					talk( 'production/'+ one +'/venue', four )
-				}
-				
-			}
-		}
-	},
-	
-	genre: function( filters, cb ) {
-		talk( 'genre', filters, cb )
-	},
-	
-	venuetype: function( filters, cb ) {
-		talk( 'venuetype', filters, cb )
 	}
+}
+
+// Venue
+module.exports.venue = function( one, two, three ) {
+	
+	if( typeof one == 'function' ) {
+		
+		// app.venue( oneCallback )
+		talk( 'venue', one )
+		
+	} else if( typeof one == 'object' && typeof two == 'function' ) {
+		
+		// app.venue( oneFilter, twoCallback )
+		talk( 'venue', one, two )
+		
+		
+	} else if( typeof one == 'string' && typeof two == 'function' ) {
+		
+		// app.venue( oneCIDN, twoCallback )
+		talk( 'venue/'+ one, two )
+		
+	} else if( typeof one == 'string' && two == undefined ) {
+		
+		// var venue = app.venue( oneCIDN )
+		return {
+			
+			// app.venue( oneCIDN ).event( fourCallback )
+			event: function( four ) {
+				talk( 'venue/'+ one +'/event', four )
+			},
+			
+			// app.venue( oneCIDN ).production( fourCallback )
+			production: function( four ) {
+				talk( 'venue/'+ one +'/production', four )
+			},
+			
+			// app.venue( oneCIDN ).room( fourCallback )
+			room: function( four ) {
+				talk( 'venue/'+ one +'/room', four )
+			},
+			
+			// app.venue( oneCIDN ).attachment( fourCallback )
+			attachment: function( four ) {
+				talk( 'venue/'+ one +'/attachment', four )
+			}
+			
+		}
+	}
+}
+
+// Production
+module.exports.production = function( one, two, three ) {
+	
+	if( typeof one == 'function' ) {
+		
+		// app.production( oneCallback )
+		talk( 'production', one )
+		
+	} else if( typeof one == 'object' && typeof two == 'function' ) {
+		
+		// app.production( oneFilter, twoCallback )
+		talk( 'production', one, two )
+		
+		
+	} else if( typeof one == 'string' && typeof two == 'function' ) {
+		
+		// app.production( oneCIDN, twoCallback )
+		talk( 'production/'+ one, two )
+		
+	} else if( typeof one == 'string' && two == undefined ) {
+		
+		// var production = app.production( oneCIDN )
+		return {
+			
+			// app.production( oneCIDN ).event( fourCallback )
+			event: function( four ) {
+				talk( 'production/'+ one +'/event', four )
+			},
+			
+			// app.production( oneCIDN ).production( fourCallback )
+			venue: function( four ) {
+				talk( 'production/'+ one +'/venue', four )
+			}
+			
+		}
+	}
+},
+
+module.exports.genre = function( filters, cb ) {
+	talk( 'genre', filters, cb )
+}
+
+module.exports.venuetype = function( filters, cb ) {
+	talk( 'venuetype', filters, cb )
 }
 
 
@@ -247,7 +244,3 @@ function talk( path, fields, cb ) {
 	// complete request
 	request.end()
 }
-
-
-// export module
-module.exports = app
